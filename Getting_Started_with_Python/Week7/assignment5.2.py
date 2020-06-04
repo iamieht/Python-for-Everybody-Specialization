79b83f7cf8bc8f88327b1f7b30011baf6d6059f3
Iván E. Hernández T <iamieht@gmail.com>
2020-06-04 19:49:08 +0200

    Create assignment5.2.py
    Final Assignment

---
+++ Getting_Started_with_Python/Week7/assignment5.2.py
@@ -0,0 +1,28 @@
+'''
+5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+Once 'done' is entered, print out the largest and smallest of the numbers. 
+If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
+Enter 7, 2, bob, 10, and 4 and match the output below. 
+'''
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        number = int(num)
+        if largest is None:
+            largest = number
+        elif number > largest:
+            largest = number
+        elif smallest is None:
+            smallest = number
+        elif number < smallest:
+            smallest = number
+    except:
+        print("Invalid input")
+        continue
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
